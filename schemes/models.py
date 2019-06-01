1e2d8c5bf38d7aa3b1627f71ea684fced77ba6a0
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2019-06-01 16:47:44 +0000

    Defined models for schemes

--- schemes/models.py
+++ schemes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Scheme(models.Model):
+    """ Scheme Post """
+    title = models.CharField(maxlength=100)
+    description = models.TextField()
+    comment = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    views = models.IntegerField(default=0)
+    completion = models.DecimalField(max_digits=3, decimal_places=2)
+    cost = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='images')
+    
+ def __unicode__(self):
+    return self.title   

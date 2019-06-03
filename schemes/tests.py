4251e9993f0204080856fb6983f3957d4f4ded86
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2019-06-03 20:13:18 +0000

    Added tests and updated views in schemes

--- schemes/tests.py
+++ schemes/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Scheme
 
-# Create your tests here.
+class SchemeTests(TestCase):
+    """
+    Here we'll define the tests
+    that we'll run against our Post model
+    """
+    
+    def test_str(self):
+        test_title = Scheme(title='A scheme')
+        self.assertEqual(str(test_title), 'A scheme')
--- schemes/views.py
+++ schemes/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Scheme
+from .forms import SchemeForm
 
-# Create your views here.
+def get_schemes(request):
+    """ 
+    Create a view that will return a list of schemes that were published prior to now 
+    and render them to the 'schemeposts.html' template
+    """
+
+    # Most recent first
+    schemes = Scheme.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, "schemeposts.html",{'schemes': schemes})
+
+def scheme_detail(request, pk):
+    """
+    Create a view that returns a single scheme object
+    based on the scheme ID (pk) and render it to the 'schemedetail.html' template.
+    Or return a 404 error if the scheme is not found.
+    """    
+    
+    scheme = get_object_or_404(Scheme, pk=pk)
+    scheme.views += 1
+    scheme.save()
+    return render(request, "schemedetail.html", {'scheme': scheme})
+
+def create_or_edit_scheme(request, pk=None):
+    """
+    Create a view that allows us to create or edit a scheme depending
+    if the Scheme ID is null or not
+    """
+    
+    scheme = get_object_or_404(Scheme, pk=pk) if pk else None
+    if request.method == "POST":
+        form = SchemeForm(request.POST, request.FILES, instance=scheme)
+        if form.is_valid():
+            scheme = form.save()
+            return redirect(scheme_detail, scheme.pk)
+    else:
+        form = SchemeForm(instance=scheme)
+        return render(request, "schemeform.html", {'form': form})
+

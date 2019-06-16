7a2da657d47c6aebc21ca01872ad7e70956012de
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2019-06-16 17:10:51 +0000

    Updated help text in models.py - schemes

--- schemes/models.py
+++ schemes/models.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 
 class Scheme(models.Model):
     """ Scheme Post with help text """
-    title = models.CharField(max_length=100, help_text="Enter scheme title (e.g. Ely Southern Bypass)")
-    description = models.TextField(help_text="Enter description of scheme (e.g. The Ely Southern Bypass is designed to Reduce Bridge Strikes)")
+    title = models.CharField(max_length=100, help_text="Enter scheme title (e.g. Ely Southern Bypass, Garden Lighting).")
+    description = models.TextField(help_text="Enter description of scheme (e.g. The Ely Southern Bypass is designed to Reduce Bridge Strikes).")
     comment = models.TextField(help_text="Enter a comment.  You can include what has been done and what needs to be doing, such as Building Works have been completed, now needs to comply with Health & Safety Requirements.  Or you can just include feedback with or without further actions, such as Good Work.")
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)

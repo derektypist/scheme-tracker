e7331c61315ac3145ade1206af612fc934bb01e1
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2019-06-23 13:48:30 +0000

    Updated checkout

--- checkout/views.py
+++ checkout/views.py
@@ -37,7 +37,7 @@ def checkout(request):
             try:
                 customer = stripe.Charge.create(
                     amount = int(total * 100),
-                    currency = "GBP",
+                    currency = "gbp",
                     description = request.user.email,
                     card = payment_form.cleaned_data['stripe_id'],
                 )

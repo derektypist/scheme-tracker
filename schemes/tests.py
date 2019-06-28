18690c07781295f66494c17c951001bde302ae68
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2019-06-28 21:52:20 +0000

    Updated tests.py in schemes

--- schemes/tests.py
+++ schemes/tests.py
@@ -1,12 +1,3 @@
 from django.test import TestCase
-from .models import Scheme
 
-class SchemeTests(TestCase):
-    """
-    Here we'll define the tests
-    that we'll run against our Scheme model
-    """
-    
-    def test_str(self):
-        test_title = Scheme(title='A scheme')
-        self.assertEqual(str(test_title), 'A scheme')
+# Create Your Tests Here
